8a22f21143c4ab05e36c0d4cfcf483f9ac4727ba
ogerasymenko <alexandr.x2@gmail.com>
2016-05-15 21:42:01 +0300

    Refactoring zp-calc.py

--- My_Py_Code/zp-calc.py
+++ My_Py_Code/zp-calc.py
@@ -1,17 +1,40 @@
-def calc():
-	curs = float(input('Введите текущий курс доллара: '))
-	usdsum = z/curs
-	print('Сумма в доларах США составляет:', int(usdsum), '$')
+def input_number():
+    loop = True
+    while loop:
+        try:
+            number = float(input('Введите сумму: '))
+            return number
+            loop = False
+        except (ValueError):
+            print('Введите сумму числом!')
+            loop = True
 
-x = float(input('Сумма: '))
+x = input_number()
 y = x * 0.2
 z = x - y
 print('Сумма с учетом налогов составляет:', int(z), 'грн')
 
+def usd_func():
+    loop = True
+    while loop:
+        try:
+            curs = float(input('Введите текущий курс доллара: '))
+            usd = z/curs
+            return usd
+            loop = False
+        except (ValueError):
+            print('Введите курс числом!')
+            loop = True
+
+
+def result():
+    usdsum = usd_func() 
+    print('Сумма в доларах США составляет:', int(usdsum), '$')
+
 usdcalc = input('Вы хотите пересчитать сумму в долларах США? ')
 
 if usdcalc == 'yes':
-	calc()
+    result()
 
 elif usdcalc == 'no':
     print('Удачи!')
@@ -20,8 +43,8 @@ elif usdcalc != 'yes':
         while usdcalc != 'yes':
             usdcalc = input('Введите yes или no: ')
             if usdcalc == 'yes':
-            	calc()
-            	break
+                result()
+                break
             elif usdcalc == 'no':
                 print('Удачи!')
                 break
