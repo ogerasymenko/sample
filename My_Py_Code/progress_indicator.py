84e3377b8d8abaf01e5d62b57b982e879d90e3ec
ogerasymenko <alexandr.x2@gmail.com>
2016-06-07 17:59:08 +0300

    Refactoring

--- My_Py_Code/progress_indicator.py
+++ My_Py_Code/progress_indicator.py
@@ -1,9 +1,16 @@
+'''
+
+Created on June 07, 2016.
+
+'''
+
 __author__ = 'sashko'
 
 import sys, time
 
 for i in range(5, 101, 5):
-    sys.stdout.write("\rThinking...%s%%" % i)
+    sys.stdout.write("\rThinking...{0}".format(i))
     sys.stdout.flush()
     time.sleep(0.2)
+    
 print('\nDone.')
