28c4df0109a006e28f13b28c5601d9b22b2efeb6
Administrator <sashko@mydomain.org>
2016-12-13 20:37:24 +0200

    Add Flask base

---
+++ My_Py_Code/Flask_test/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('main.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
